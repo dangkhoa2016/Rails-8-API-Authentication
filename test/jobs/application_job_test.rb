dc2e460a04f0032f3c389b9613784046f15d6223
Dang Khoa <i.am@dangkhoa.dev>
2026-07-10 10:00:00 +0700

    refactor: remove FailureMiddleware and add unit tests for jobs, mailers, models
    - Delete lib/failure_middleware.rb and remove commented-out require
    - Add unit tests for ApplicationJob, CleanExpiredJwtDenylistsJob,
      ApplicationMailer, CoverageReportRedirectMiddleware, JwtDenylist, and User
    - Normalize test_helper and disable parallel tests under COVERAGE

---
+++ test/jobs/application_job_test.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class ApplicationJobTest < ActiveSupport::TestCase
+  test "inherits from active job base" do
+    assert_equal ActiveJob::Base, ApplicationJob.superclass
+  end
+end
