18d63d389b61dc883c9adb93415809d9ea2c1081
Đăng Khoa <i.am@dangkhoa.dev>
2026-07-15 15:17:10 +0000

    refactor: remove unused Devise controllers and obsolete tests
    - Remove default Confirmations, Unlocks, and OmniAuth controllers
    - Remove obsolete controller tests for deleted Devise controllers
    - Fix error key inconsistency in UsersController (singular error key)
    - Simplify find_user to use User.find directly
    - Update tests to match the API error contract

--- test/controllers/users/confirmations_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "test_helper"
-
-class Users::ConfirmationsControllerTest < ActionDispatch::IntegrationTest
-  test "inherits from devise confirmations controller" do
-    assert_equal Devise::ConfirmationsController, Users::ConfirmationsController.superclass
-  end
-end
